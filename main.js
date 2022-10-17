e287df99e2357dd0b73c267c7378bd87a499ef30
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2022-10-17 06:03:23 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function clickedSearchIcon() {
+  document.getElementsByClassName("navBarLeftSection")[0].style.display =
+    "none";
+  document.getElementsByClassName("navBarRightSection")[0].style.display =
+    "none";
+  document.getElementsByClassName("navBarMiddleSection")[0].style.display =
+    "flex";
+  document.getElementById("backIconResponsive").style.display = "flex";
+  document.getElementsByClassName("naviBar")[0].style.justifyContent = "center";
+}
+
+function clickedBackIcon() {
+  document.getElementById("backIconResponsive").style.display = "none";
+  document.getElementsByClassName("navBarLeftSection")[0].style.display =
+    "flex";
+  document.getElementsByClassName("navBarRightSection")[0].style.display =
+    "flex";
+  document.getElementsByClassName("naviBar")[0].style.justifyContent =
+    "space-between";
+  document.getElementsByClassName("navBarMiddleSection")[0].style.display =
+    "none   ";
+}
